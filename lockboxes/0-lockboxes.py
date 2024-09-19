feea452fd65f03897eb92845421d56ba4bc58c9e
freddyrosa16 <freddy.rosa16@gmail.com>
2024-09-19 11:07:50 -0400

    Writing Documentation

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+""" Lockboxes """
+
+
 def canUnlockAll(boxes):
+    """
+    - boxes is a list of lists
+    - A key with the same number as a box opens that box
+    - You can assume all keys will be positive integers
+    - The first box boxes[0] is unlocked
+    - Return True if all boxes can be opened, else return False
+    """
     unlocked = set()
     to_explore = [0]
 
